6d09efaa0667006b1635336bfa5169c780aeffc7
DiOrator <oratorsconnect.gmail.com>
2025-09-27 17:51:36 +0100

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
       <div className='hero-center'>
         <h1>Discover the Finest Accessories</h1>
         <p>Bracelets, watches, perfumes, and more – crafted to perfection.</p>
-        <a href='https://wa.me/2349025799243' className='btn'>
+        <a href='https://wa.me/2347040284996' className='btn'>
           Order Now
         </a>
       </div>
